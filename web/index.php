364ef4b87d6d577584cab1a7812f35a801ce389f
red545 <nik@redl.wien>
2018-09-08 14:29:44 +0200

    starting design

---
+++ web/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+
+$loader = require __DIR__.'/../config/autoload.php';
+require_once __DIR__.'/../src/MicroKernel.php';
+
+$kernel = MicroKernel::fromEnvironment();
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
+
