fbfe613d2c862602984e8294033467884502df78
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2018-09-15 19:07:12 +0200

    Use minichart for birth/death

--- src/AppBundle/Controller/MapController.php
+++ src/AppBundle/Controller/MapController.php
@@ -47,7 +47,6 @@ extends CrudController
                 continue;
             }
 
-
             $key = $row['latitude'] . ':' . $row['longitude'];
 
             if (!array_key_exists($key, $values)) {
@@ -67,11 +66,14 @@ extends CrudController
             if (!in_array($row['person_id'], $values[$key]['person_ids']['birth'])
                 && !in_array($row['person_id'], $values[$key]['person_ids']['death']))
             {
-                $values[$key]['persons'][] = sprintf('<a href="%s">%s</a>',
-                                                     htmlspecialchars($this->generateUrl('person', [
-                                                        'id' => $row['person_id'],
-                                                    ])),
-                                                    $row['lastname'] . ', ' . $row['firstname']);
+                $values[$key]['persons'][] = [
+                    'id' => $row['person_id'],
+                    'label' => sprintf('<a href="%s">%s</a>',
+                                       htmlspecialchars($this->generateUrl('person', [
+                                           'id' => $row['person_id'],
+                                       ])),
+                                       htmlspecialchars($row['lastname'] . ', ' . $row['firstname'], ENT_COMPAT, 'utf-8')),
+                ];
             }
 
             $values[$key]['person_ids'][$row['type']][] = $row['person_id'];
@@ -79,13 +81,31 @@ extends CrudController
 
         $values_final = [];
         $max_count = 0;
+
         foreach ($values as $key => $value) {
+            $idsByType = & $values[$key]['person_ids'];
+
+            $buildRow = function ($entry) use ($idsByType) {
+                $ret = $entry['label'];
+
+                $append = '';
+                if (in_array($entry['id'], $idsByType['birth'])) {
+                    $append .= '*';
+                }
+                if (in_array($entry['id'], $idsByType['death'])) {
+                    $append .= '+';
+                }
+
+                return $ret . ('' !== $append ? ' ' . $append : '');
+            };
+
             $count_entries = count($value['persons']);
+
             if ($count_entries <= $maxDisplay) {
-                $entry_list = implode('<br />', $value['persons']);
+                $entry_list = implode('<br />', array_map($buildRow, $value['persons']));
             }
             else {
-                $entry_list = implode('<br />', array_slice($value['persons'], 0, $maxDisplay - 1))
+                $entry_list = implode('<br />', array_map($buildRow, array_slice($value['persons'], 0, $maxDisplay - 1)))
                             . sprintf('<br />... (%d more)', $count_entries - $maxDisplay);
             }
 
@@ -93,10 +113,11 @@ extends CrudController
                 $value['latitude'], $value['longitude'],
                 $value['place'],
                 $entry_list,
-                $count = count($value['person_ids']['birth']) + count($value['person_ids']['death']),
+                $count_birth = count($value['person_ids']['birth']),
+                $count_death = count($value['person_ids']['death'])
             ];
 
-            if ($count > $max_count) {
+            if (($count = $count_birth + $count_death) > $max_count) {
                 $max_count = $count;
             }
         }
@@ -108,7 +129,7 @@ extends CrudController
             'disableClusteringAtZoom' => 7,
             'maxCount' => $max_count,
             'showHeatMap' => true,
-            'markerStyle' => 'circle',
+            'markerStyle' => 'pie', // 'circle' is other option
             'bounds' => [
                 [ 60, -120 ],
                 [ -15, 120 ],
