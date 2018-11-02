164bbe3a6bf01f89e9c191393a192e72e8694aa3
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2018-11-02 12:35:58 +0100

    Add register and password recover

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,12 +4,15 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Table(name="User")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
- */
+ * @UniqueEntity(fields="email", message="This E-Mail is already registered")
+ * */
 class User
 implements UserInterface, \Serializable
 {
@@ -17,6 +20,11 @@ implements UserInterface, \Serializable
         'ROLE_EXPERT' => 0x10, // access to works
         'ROLE_ADMIN' => 0x200, // additional rights to edit / delete / publish
     ];
+
+    public static function generateToken($numBytes = 20)
+    {
+        return rtrim(strtr(base64_encode(random_bytes($numBytes)), '+/', '-_'), '=');
+    }
 
     /**
      * @ORM\Column(type="integer")
@@ -31,9 +39,20 @@ implements UserInterface, \Serializable
     private $email;
 
     /**
+     * @Assert\NotBlank()
+     * @Assert\Length(min=6, max=4096)
+     */
+    private $plainPassword;
+
+    /**
      * @ORM\Column(name="pwd", type="string", length=64)
      */
     private $password;
+
+    /**
+    * @ORM\Column(name="recover", type="string", length=32, nullable=true)
+    */
+    private $confirmationToken;
 
     /**
      * @ORM\Column(type="integer")
@@ -43,11 +62,23 @@ implements UserInterface, \Serializable
     /**
      * @ORM\Column(type="integer")
      */
-    private $privs;
+    private $privs = 0;
 
     public function __construct()
     {
         $this->status = 0;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
     }
 
     public function getUsername()
@@ -60,6 +91,16 @@ implements UserInterface, \Serializable
         // you *may* need a real salt depending on your encoder
         // see section on salt below
         return null;
+    }
+
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword($password)
+    {
+        $this->plainPassword = $password;
     }
 
     /**
@@ -79,6 +120,25 @@ implements UserInterface, \Serializable
         if ('' !== $password) {
             $this->password = $password;
         }
+    }
+
+    public function getConfirmationToken()
+    {
+        return $this->confirmationToken;
+    }
+
+    public function setConfirmationToken($confirmationToken)
+    {
+        $this->confirmationToken = $confirmationToken;
+
+        return $this;
+    }
+
+    public function setGeneratedConfirmationToken()
+    {
+        $this->setConfirmationToken(self::generateToken());
+
+        return $this;
     }
 
     public function getRoles()
