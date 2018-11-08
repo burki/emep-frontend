c8dd5a806fe14da6d633b81ad15114f720f4dd04
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2018-11-08 08:36:02 +0100

    Add /person/gdf

--- src/AppBundle/Command/ExportGdfCommand.php
+++ src/AppBundle/Command/ExportGdfCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-// src/AppBundle/Command/EntityEnhanceCommand.php
+// src/AppBundle/Command/ExportGdfCommand.php
 namespace AppBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -11,7 +11,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
-class ExportGdfCommand extends ContainerAwareCommand
+class ExportGdfCommand
+extends ContainerAwareCommand
 {
     protected function configure()
     {
@@ -21,20 +22,59 @@ class ExportGdfCommand extends ContainerAwareCommand
             ->addArgument(
                 'type',
                 InputArgument::REQUIRED,
-                'what you want to export (person)'
+                'what you want to export (person or location)'
             )
-            ;
+            ->addOption(
+                'save-export-path',
+                false,
+                InputOption::VALUE_OPTIONAL,
+                'Write output to app.export.path/type.gdf'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $saveExportPath = $input->getOption('save-export-path');
+        if ($saveExportPath !== false) {
+            $container = $this->getContainer();
+            $exportPath = $container->hasParameter('app.export.path')
+                ? $container->getParameter('app.export.path')
+                : $container->get('kernel')->getProjectDir() . '/../site/htdocs/uploads/export';
+
+            if (!file_exists($exportPath)) {
+                $output->writeln(sprintf('<error>app.export.path: %s does not exist</error>',
+                                         $exportPath));
+                return 1;
+            }
+
+            $type = $input->getArgument('type');
+            if (!in_array($type, [ 'person', 'location' ])) {
+                $output->writeln(sprintf('<error>invalid type: %s</error>',
+                                         $input->getArgument('type')));
+                return 1;
+            }
+
+            $fnameLock = realpath($exportPath) . DIRECTORY_SEPARATOR . $type . '.lock';
+            if (file_exists($fnameLock)) {
+                $output->writeln(sprintf('<error>Execution is alread in progress (%s)</error>',
+                                         $fnameLock));
+                return 2;
+            }
+
+            touch($fnameLock);
+
+            $fnameFull = realpath($exportPath) . DIRECTORY_SEPARATOR . $type . '.gdf';
+
+            $output = new \Symfony\Component\Console\Output\StreamOutput($fp = fopen($fnameFull, 'w', false));
+        }
+
         switch ($input->getArgument('type')) {
             case 'person':
-                return $this->exportPersonGdf();
+                $res = $this->exportPersonGdf($output);
                 break;
 
             case 'location':
-                return $this->exportLocationGdf();
+                $res = $this->exportLocationGdf();
                 break;
 
             default:
@@ -42,6 +82,13 @@ class ExportGdfCommand extends ContainerAwareCommand
                                          $input->getArgument('type')));
                 return 1;
         }
+
+        if ($saveExportPath !== false) {
+            fclose($fp);
+            unlink($fnameLock);
+        }
+
+        return $res;
     }
 
     private function setEdges(&$edges, $shared_ids, $weighted = false)
@@ -68,7 +115,7 @@ class ExportGdfCommand extends ContainerAwareCommand
         }
     }
 
-    protected function exportPersonGdf()
+    protected function exportPersonGdf($output)
     {
         $em = $this->getContainer()->get('doctrine')->getManager();
         $qb = $em->createQueryBuilder();
@@ -79,12 +126,11 @@ class ExportGdfCommand extends ContainerAwareCommand
             ])
             ->from('AppBundle:Person', 'P')
             ->leftJoin('P.exhibitions', 'E')
-            // ->leftJoin('P.catalogueEntries', 'IE')
             ->where('P.status <> -1 AND E.status <> -1')
             ->groupBy('P.id') // for Count
             ->having('numExhibitionSort >= 2')
             ->orderBy('numExhibitionSort', 'DESC')
-            // ->setMaxResults(50)
+            // ->setMaxResults(5)
             ;
 
         $query = $qb->getQuery();
@@ -95,7 +141,7 @@ class ExportGdfCommand extends ContainerAwareCommand
             $nodes[$person->getId()] = $person->getFullname(true);
         }
 
-        echo 'nodedef>name VARCHAR,label VARCHAR' . "\n";
+        $output->writeln('nodedef>name VARCHAR,label VARCHAR');
         $data = [];
         foreach ($nodes as $nodeId => $nodeLabel) {
             // echo implode(',', [ $nodeId, $nodeLabel ]), "\n";
@@ -108,7 +154,7 @@ class ExportGdfCommand extends ContainerAwareCommand
             fputcsv($fp, $fields);
         }
         rewind($fp); // Set the pointer back to the start
-        echo stream_get_contents($fp); // Fetch the contents of our CSV
+        $output->writeln(stream_get_contents($fp)); // Fetch the contents of our CSV
         fclose($fp); // Close our pointer and free up memory and /tmp space
 
         $qb = $em->createQueryBuilder();
@@ -147,9 +193,9 @@ class ExportGdfCommand extends ContainerAwareCommand
             $this->setEdges($edges, $persons);
         }
 
-        echo 'edgedef>node1 VARCHAR,node2 VARCHAR, weight DOUBLE' . "\n";
+        $output->writeln('edgedef>node1 VARCHAR,node2 VARCHAR, weight DOUBLE');
         foreach ($edges as $edgeKey => $edgeCount) {
-            echo join(',', [ $edgeKey, $edgeCount ]), "\n";
+            $output->writeln(join(',', [ $edgeKey, $edgeCount ]));
         }
     }
 
---
+++ src/AppBundle/Controller/GraphController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Cocur\BackgroundProcess\BackgroundProcess;
+
+/**
+ *
+ */
+class GraphController
+extends Controller
+{
+    static function getPhpBin()
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            // we don't seem to have a reliable way on windows, so assume it is in same dir as php.ini
+            return dirname(php_ini_loaded_file()) . DIRECTORY_SEPARATOR . 'php.exe';;
+        }
+
+
+        $phpFinder = new \Symfony\Component\Process\PhpExecutableFinder;
+        if (!$phpPath = $phpFinder->find()) {
+            throw new \Exception('The php executable could not be found, add it to your PATH environment variable and try again');
+        }
+
+        return $phpPath;
+    }
+
+    /**
+     * @Route("/person/gdf", name="person-gdf")
+     */
+    public function gdfAction(Request $request)
+    {
+        $route = $request->get('_route');
+
+        $exportPath = $this->container->hasParameter('app.export.path')
+            ? $this->getParameter('app.export.path')
+            : $this->get('kernel')->getProjectDir() . '/../site/htdocs/uploads/export';
+
+        if (false === realpath($exportPath)) {
+            throw new \InvalidArgumentException($exportPath . ' does not exist');
+        }
+
+        $fname = 'person';
+
+        $fnameFull = realpath($exportPath) . DIRECTORY_SEPARATOR . $fname . '.gdf';
+        $fnameLock = realpath($exportPath) . DIRECTORY_SEPARATOR . $fname . '.lock';
+
+        $regenerate = true;
+
+        if (file_exists($fnameFull)) {
+            $regenerate = false;
+        }
+
+        if ($regenerate || file_exists($fnameLock)) {
+            if (!file_exists($fnameLock)) {
+                $phpBin = self::getPhpBin();
+
+                $builder = new ProcessBuilder();
+                $builder->setPrefix($phpBin);
+                $command = $builder->setArguments([
+                        realpath(__DIR__ .'/../../../bin/console'),
+                        'export:gdf',
+                        'person',
+                        '--save-export-path'
+                    ])
+                    ->getProcess()
+                    ->getCommandLine();
+
+                $process = new BackgroundProcess($command);
+                $process->run();
+            }
+
+            $response = new Response();
+
+            $response->setStatusCode(200);
+            $url = $this->generateUrl($route, [],
+                                      \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL);
+
+
+            $response->setContent(sprintf('<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="refresh" content="5;url=%1$s" />
+        <title>Redirecting to %1$s</title>
+    </head>
+    <body>
+        Regeneration of %2$s is currently in progress. Please wait.
+    </body>
+</html>',
+            htmlspecialchars($url, ENT_QUOTES, 'UTF-8'), $fname . '.gdf'));
+
+            // $response->headers->set('Refresh', '5; url=' . $url);
+
+            return $response;
+        }
+
+        if (!file_exists($fnameFull)) {
+           throw $this->createNotFoundException($fname . '.gdf' . ' does not exist');
+        }
+
+        return new \Symfony\Component\HttpFoundation\StreamedResponse(
+            function () use ($fnameFull) {
+                readfile($fnameFull);
+            }, Response::HTTP_OK, [
+                'Content-Type' => 'text/plain',
+                'Content-Disposition' => 'attachment; filename="' . $fname . '.gdf' . '"'
+            ]
+        );
+    }
+}
