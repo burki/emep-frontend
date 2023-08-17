ac5488c21b6e37f1c0c078983e1fcf82d15c016d
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2023-08-17 17:24:35 +0200

    Move to RoutingConfigurator

--- src/MicroKernel.php
+++ src/MicroKernel.php
@@ -4,14 +4,12 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
-use Symfony\Component\Routing\RouteCollectionBuilder;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 // see https://github.com/ikoene/symfony-micro
-class MicroKernel
+final class MicroKernel
 extends Kernel
 {
-    const CONFIG_EXTS = '.{yaml}';
-
     /*
      * Set an Environment Variable in Apache Configuration
      *   SetEnv APP_ENVIRONMENT prod
@@ -44,7 +42,7 @@ extends Kernel
     /*
      * {@inheritDoc}
      */
-    public function registerBundles()
+    public function registerBundles(): iterable
     {
         $bundles = [
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
@@ -106,17 +104,17 @@ extends Kernel
     }
 
     // see https://github.com/symfony/symfony-standard/blob/master/app/AppKernel.php
-    public function getCacheDir()
+    public function getCacheDir(): string
     {
         return $this->getProjectDir().'/var/cache/'.$this->getEnvironment();
     }
 
-    public function getLogDir()
+    public function getLogDir(): string
     {
         return $this->getProjectDir().'/var/logs';
     }
 
-    public function getConfigDir()
+    public function getConfigDir(): string
     {
         return $this->getProjectDir().'/config';
     }
@@ -132,33 +130,35 @@ extends Kernel
 
     /*
      * {@inheritDoc}
+     *
+     * use
+     *      bin/console debug:router
+     * to show all your routes
+     *
      */
-    protected function configureRoutes(RouteCollectionBuilder $routes)
+    protected function configureRoutes(RoutingConfigurator $routes)
     {
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
-            $routes->mount('/_wdt', $routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml'));
-            $routes->mount(
-                '/_profiler',
-                $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml')
-            );
+        if (in_array($this->getEnvironment(), [ 'dev', 'test' ], true)) {
+            $routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml')->prefix('/_wdt');
+            $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml')->prefix('/_profiler');
 
             // Preview error pages through /_error/{statusCode}
             //   see http://symfony.com/doc/current/cookbook/controller/error_pages.html
             // Note: not sure why this is mapped to /_error/_error/{code}.{_format} as can be seen by
-            //   bin/console debug:router | grep twig
-            // ->_twig_error_test            ANY      ANY      ANY    /_error/_error/{code}.{_format}
-
-            $routes->mount(
-                '/_error',
-                $routes->import('@FrameworkBundle/Resources/config/routing/errors.xml')
-            );
+            //   bin/console debug:router | grep error
+            // -> _preview_error  ANY      ANY      ANY    /_error/_error/{code}.{_format}
+            $routes->import('@FrameworkBundle/Resources/config/routing/errors.xml')->prefix('/_error');
         }
 
         // our controllers
         $confDir = $this->getConfigDir();
 
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+        $routes->import('../config/{routes}/*.yaml');
+
+        if (is_file($confDir.'/routes.yaml')) {
+            $routes->import($confDir.'/routes.yaml');
+        } else {
+            $routes->import($confDir.'/{routes}.php');
+        }
     }
 }
