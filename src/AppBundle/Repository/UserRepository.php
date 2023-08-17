f2fca40297cf5531afd0bd8b83cffca9931d521c
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2023-08-17 17:23:22 +0200

    Prepare for Symfony 6

--- src/AppBundle/Repository/UserRepository.php
+++ src/AppBundle/Repository/UserRepository.php
@@ -7,17 +7,25 @@ namespace AppBundle\Repository;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Doctrine\ORM\EntityRepository;
 
+use AppBundle\Entity\User;
+
 class UserRepository
 extends EntityRepository
 implements UserLoaderInterface
 {
-    // see https://symfony.com/doc/3.4/security/entity_provider.html
-    public function loadUserByUsername($username)
+    // see https://symfony.com/doc/5.4/security/user_providers.html
+    public function loadUserByIdentifier(string $usernameOrEmail): ?User
     {
         return $this->createQueryBuilder('u')
             ->where('u.email = :email AND u.status <> -1')
-            ->setParameter('email', $username)
+            ->setParameter('email', $usernameOrEmail)
             ->getQuery()
             ->getOneOrNullResult();
+    }
+
+    /** @deprecated since Symfony 5.3 */
+    public function loadUserByUsername(string $usernameOrEmail): ?User
+    {
+        return $this->loadUserByIdentifier($usernameOrEmail);
     }
 }
